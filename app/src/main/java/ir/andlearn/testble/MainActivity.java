b1a45c01ae262d8b469f6b200f82999bbe3285fb
navid <navidrct@gmail.com>
2016-06-13 14:01:53 +0330

    my test for branch

--- app/src/main/java/ir/andlearn/testble/MainActivity.java
+++ app/src/main/java/ir/andlearn/testble/MainActivity.java
@@ -27,7 +27,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        ble = new BLE();
+        ble.check_BLE_Support(this);
+        ble.turn_ble_on(this);
 
 
 
