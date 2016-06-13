2c7fc6f04383c3b52b288dd46f8145500d577c57
navidrct <navidrct@gmail.com>
2016-06-13 18:25:28 +0330

    my test for branch

--- app/src/main/java/ir/andlearn/testble/MainActivity.java
+++ app/src/main/java/ir/andlearn/testble/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Log.d(TAG, "onCreate: ali ansari");
 
 
 
